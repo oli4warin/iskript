5beaeb97c9c759811977678a98e1c060b47c4458
olidec <69003898+olidec@users.noreply.github.com>
2021-11-23 21:11:57 +0100

    Update hash_test.py

--- in_progress/code/hash_test.py
+++ in_progress/code/hash_test.py
@@ -2,11 +2,11 @@ import hashlib
 
 string_to_hash = input("Please enter your text to hash: ")
 
-def my_hash_md5(input):
-    return hashlib.md5(input.encode()).hexdigest()
+def hash_md5(text):
+    return hashlib.md5(text.encode()).hexdigest()
 
-def my_hash_sha1(input):
-    return hashlib.sha1(input.encode()).hexdigest()
+def hash_sha1(text):
+    return hashlib.sha1(text.encode()).hexdigest()
 
-print("The md5 hashed output is: " + my_hash_md5(string_to_hash))
-print("The sha1 hashed output is: " + my_hash_sha1(string_to_hash))
+print("The md5 hashed output is: " + hash_md5(string_to_hash))
+print("The sha1 hashed output is: " + hash_sha1(string_to_hash))
